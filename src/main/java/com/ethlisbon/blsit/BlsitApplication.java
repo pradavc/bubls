2e103d33aa8f38b51a45452d8ea0c12b5b215c8b
xyz <11524882+xyzmap@users.noreply.github.com>
2021-10-23 12:53:06 +0100

    Initial commit

---
+++ src/main/java/com/ethlisbon/blsit/BlsitApplication.java
@@ -0,0 +1,13 @@
+package com.ethlisbon.blsit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlsitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlsitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethlisbon/blsit/BlsitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethlisbon.blsit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlsitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
